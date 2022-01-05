a72c28c86875e2d5628e3f629018934b6d158e14
arman <arman_babaee68@yahoo.com>
2022-01-05 21:18:41 +0330

    update

--- public/index.php
+++ public/index.php
@@ -1,27 +1,16 @@
 <?php
-
+set_time_limit(0);
 require('../vendor/autoload.php');
-
-$url = isset($_GET['url']) ? $_GET['url'] : null;
-
-function send_json($data)
-{
-    header('Content-Type: application/json');
-    echo json_encode($data, JSON_PRETTY_PRINT);
-    exit;
-}
-
-if (!$url) {
-    send_json([
-        'error' => 'No URL provided!'
-    ]);
-}
 
 $youtube = new \YouTube\YouTubeDownloader();
 $youtube->getBrowser()->setCookieFile('./cookies.txt');
 $youtube->getBrowser()->setUserAgent('Mozilla/5.0 (Windows NT 6.3; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0');
+
+$videoId = isset($_GET['v']) ? $_GET['v'] : null;
+$urlGet = "https://www.youtube.com/watch?v=$videoId";
 try {
-    $links = $youtube->getDownloadLinks($url);
+    $links = $youtube->getDownloadLinks($urlGet);
+
 
     if ($allFormats = $links->getAllFormats()) {
 
@@ -34,6 +23,10 @@ try {
 
     } else {
         echo 'No links found';
+    }
+
+    if ($url == false) {
+        die("No url provided");
     }
 
     $youtube = new \YouTube\YouTubeStreamer();

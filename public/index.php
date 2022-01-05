a00b9b25eb86167bc6cc0ba566c3def748608ac9
arman <arman_babaee68@yahoo.com>
2022-01-05 20:59:02 +0330

    update

---
+++ public/index.php
@@ -0,0 +1,48 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$url = isset($_GET['url']) ? $_GET['url'] : null;
+
+function send_json($data)
+{
+    header('Content-Type: application/json');
+    echo json_encode($data, JSON_PRETTY_PRINT);
+    exit;
+}
+
+if (!$url) {
+    send_json([
+        'error' => 'No URL provided!'
+    ]);
+}
+
+$youtube = new \YouTube\YouTubeDownloader();
+$youtube->getBrowser()->setCookieFile('./cookies.txt');
+$youtube->getBrowser()->setUserAgent('Mozilla/5.0 (Windows NT 6.3; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0');
+try {
+    $links = $youtube->getDownloadLinks($url);
+
+    if ($allFormats = $links->getAllFormats()) {
+
+        foreach ($allFormats as $format){
+            if ($format->qualityLabel == '720p' && $format->audioQuality != null){
+                $url = $format->url;
+                break;
+            }
+        }
+
+    } else {
+        echo 'No links found';
+    }
+
+    $youtube = new \YouTube\YouTubeStreamer();
+    $youtube->stream($url);
+
+
+} catch (\YouTube\Exception\YouTubeException $e) {
+
+    send_json([
+        'error' => $e->getMessage()
+    ]);
+}
